58dfab6af5047325493ee1b30f4735b90ddcf178
Ester <ester.zea.maldonado@gmail.com>
2024-08-17 13:33:14 +0200

    WIP-Trabajando en el CRUD

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../interfaces/cliente';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  listaClientes: Cliente[] = [
+    { id: 1,
+      dni: "44587963L",
+      nombre: "Ana",
+      apellido: "Martín Prueba1",
+      ciudad: "Bacelona",
+      email: "prueba1@.gmail.com",
+      telefono: 652374521
+    },
+    { id: 2,
+      dni: "44758563F",
+      nombre: "Pepe",
+      apellido: "Osuna Prueba2",
+      ciudad: "Madrid",
+      email: "prueba2@.gmail.com",
+      telefono: 412789634
+    },
+  ]
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
---
+++ src/app/interfaces/cliente.ts
@@ -0,0 +1,9 @@
+export interface Cliente {
+  id: number;
+  dni: string;
+  nombre: string;
+  apellido: string;
+  ciudad: string;
+  email: string;
+  telefono: number;
+}
